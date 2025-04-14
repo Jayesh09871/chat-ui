dad8fa1c0326b157234b09e744761076e8076a73
Jayesh09871 <jayeshkakhani29@gmail.com>
2025-04-14 12:28:48 +0530

    deploy frontend

--- socket/server.js
+++ socket/server.js
@@ -5,7 +5,7 @@ const server = http.createServer(app);
 const { Server } = require('socket.io');
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://chat-ui-ht79.vercel.app",
     methods: ["GET", "POST"]
   }
 });
